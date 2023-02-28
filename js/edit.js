6d860da23a44d920117f3ecd9a250908a872553e
an4morph <avgustin456@gmail.com>
2023-02-28 21:03:29 +0600

    fill inputs on edit

--- js/edit.js
+++ js/edit.js
@@ -1,5 +1,11 @@
+import { formatDate } from "./utils/index.js"
 const editData = JSON.parse(localStorage.getItem('editData'))
 
 const [nameInput, number1Input, number2Input, dateInput] = 
   ['name', 'number1', 'number2', 'date']
     .map(name => document.querySelector(`#${name}-input`))
+
+nameInput.value = editData.name
+number1Input.value = editData.number1
+number2Input.value = editData.number2
+dateInput.value = formatDate(editData.birthday, 'input')
