474cd634f464c2b1a2163c737e99d16dd2b538f9
an4morph <avgustin456@gmail.com>
2023-02-28 20:36:16 +0600

    add localstorage to edit

--- js/detail.js
+++ js/detail.js
@@ -1,10 +1,13 @@
 import { getContactDetail } from './utils/api.js'
 import { renderContactPage } from './utils/creators.js'
 
+const editButton = document.querySelector('.edit-btn')
 const id = new URLSearchParams(window.location.search).get('id')
+let contactData = null
 
 getContactDetail(id)
   .then((data) => {
+    contactData = data
     renderContactPage(data)
   })
   .catch(() => {
@@ -13,5 +16,10 @@ getContactDetail(id)
   .finally(() => {
     // убирать loader
   })
+
+editButton.addEventListener('click', () => {
+  localStorage.setItem('editData', JSON.stringify(contactData))
+  // window.location.href = `/edit.html?id=${id}`
+})
 
   
