42226001f7003c723969d793e563e071f56e6cab
an4morph <avgustin456@gmail.com>
2023-02-28 21:13:34 +0600

    create object to udate

--- js/edit.js
+++ js/edit.js
@@ -1,5 +1,7 @@
 import { formatDate } from "./utils/index.js"
+
 const editData = JSON.parse(localStorage.getItem('editData'))
+const saveBtn = document.querySelector('.save-btn')
 
 const [nameInput, number1Input, number2Input, dateInput] = 
   ['name', 'number1', 'number2', 'date']
@@ -9,3 +11,15 @@ nameInput.value = editData.name
 number1Input.value = editData.number1
 number2Input.value = editData.number2
 dateInput.value = formatDate(editData.birthday, 'input')
+
+saveBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  
+  const updatedContact = {
+    name: nameInput.value,
+    number1: number1Input.value,
+    number2: number1Input.value,
+    birthday: new Date(dateInput.value).toISOString()
+  }
+  console.log(updatedContact)
+})
